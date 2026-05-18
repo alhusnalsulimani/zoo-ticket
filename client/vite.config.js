e445cbecd9f75a9391787c159ede388f8a1cb4e0
Bayan Said <s26S2179@nct.edu.om>
2026-05-18 22:11:54 +0400

    Fix Vite environment issue and update project files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom', //simulates a browser DOM inside Node.js
-    setupFiles: './src/setupTests.js',
-    assets: ['**/*.{jpg,jpeg,png,gif,svg}'], // ← handles image imports
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: false,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      host: 'localhost',
+      port: 5173,
+      protocol: 'ws',
+    }
   }
-
 })
